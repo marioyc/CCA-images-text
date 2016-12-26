b813e3175e4acfa501d8980ddfd0de835d486496
Ubuntu <ubuntu@ip-172-31-29-220.ec2.internal>
2016-12-26 16:36:26 +0000

    add installation script

---
+++ aws_install_ubuntu14.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get upgrade
+
+apt-get install -y python2.7-dev python-pip libblas-dev liblapack-dev gfortran
+
+pip install numpy scipy
+pip install scikit-learn
+
+pip install gensim
