4599db5d712d3dabb27b3f2cfb54d0884cc0d4b1
marioyc <ycmario@gmail.com>
2017-01-05 20:55:10 +0000

    add nltk

--- aws_install_ubuntu14.sh
+++ aws_install_ubuntu14.sh
@@ -6,7 +6,8 @@ apt-get install -y python2.7-dev python-pip libblas-dev liblapack-dev gfortran p
 pip install numpy scipy
 pip install scikit-learn
 
-pip install gensim
+pip install gensim nltk
+python -m nltk.downloader all
 #wget https://s3.amazonaws.com/mordecai-geo/GoogleNews-vectors-negative300.bin.gz
 wget http://mattmahoney.net/dc/text8.zip
 apt-get install -y unzip
