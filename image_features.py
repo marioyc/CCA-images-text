c9fd22b9088604870e632ab04d78addd764bf317
marioyc <ycmario@gmail.com>
2017-01-10 20:51:44 +0000

    load model only once

--- image_features.py
+++
@@ -1,8 +0,0 @@
-from keras.applications.vgg16 import VGG16, preprocess_input
-
-def vgg16_features(img_array):
-    model = VGG16(weights='imagenet', include_top=False)
-    img_array = preprocess_input(img_array)
-    features = model.predict(img_array)
-    features = features.reshape(img_array.shape[0], -1)
-    return features
--- main.py
+++ main.py
@@ -1,74 +1,81 @@
 from gensim.models import word2vec
+from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.preprocessing import image
 from pycocotools.coco import COCO
 from scipy.spatial import distance
 from sklearn.cross_decomposition import CCA
+import progressbar
 import nltk
 import numpy as np
-import progressbar
 import time
 
 import cca
-import image_features
-
-K_IMG = 500
 
 annFile = 'annotations/captions_train2014.json'
 coco_train = COCO(annFile)
 ids = coco_train.getAnnIds()
 annotations = coco_train.loadAnns(ids)
-print len(annotations)
 
 cont = {}
-print "Counting word frequencies"
+print "Count word frequencies, number of annotations =", len(annotations)
+img_words = {}
 bar = progressbar.ProgressBar()
 for ann in bar(annotations):
     caption = ann['caption']
+    image_id = ann['image_id']
+
+    if image_id not in img_words:
+        img_words[image_id] = set()
+
     for w in nltk.word_tokenize(caption):
         w = w.lower()
+        img_words[image_id].add(w)
         if w in cont:
             cont[w] += 1
         else:
             cont[w] = 1
+print "Training: number of images =", len(img_words)
 
 model = word2vec.Word2Vec.load_word2vec_format('text.model.bin', binary=True)
+net = VGG16(weights='imagenet', include_top=False)
 img_list = []
 tag_list = []
 counter_not_in_vocab = 0
-print "Training: loading images, choose tag for each image"
+print "Training: calculate image features, choose tag for each image"
 bar = progressbar.ProgressBar()
-for ann in bar(annotations[:8000]):
-    file_name = coco_train.imgs[ ann['image_id'] ]['file_name']
+for image_id, words in bar(img_words.iteritems()):
+    file_name = coco_train.imgs[image_id]['file_name']
     img = image.load_img('train2014/' + file_name, target_size=(224, 224))
 
-    caption = ann['caption']
     tag, best = '', -1
-    for w in nltk.word_tokenize(caption):
-        w = w.lower()
+    for w in words:
         if w in model.vocab and (best == -1 or cont.get(w,0) < best):
             tag, best = w, cont.get(w,0)
 
     if best != -1:
-        img_list.append(image.img_to_array(img))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+        features = net.predict(img)
+        features = features.reshape(-1)
+        img_list.append(features)
         tag_list.append(model[tag])
     else:
         counter_not_in_vocab += 1
+img_features = np.array(img_list)
+print img_features.shape
+del img_list
 tag_features = np.array(tag_list)
+print tag_features.shape
 del tag_list
 
-print "Caption with no word in vocab:", counter_not_in_vocab
-
-print "Training: calculate image embeddings"
-start = time.time()
-img_features = image_features.vgg16_features(np.array(img_list))
-end = time.time()
-del img_list
-print 'Time: {0:.4f}m'.format((end - start) / 60)
+print "Images with no word in vocab:", counter_not_in_vocab
 
 print "Training: fit CCA"
 start = time.time()
-img_features = img_features[:,:K_IMG]
 W_img, W_tag = cca.cca(img_features, tag_features, numCC=15)
+np.save('W_img', W_img)
+np.save('W_tag', W_tag)
 end = time.time()
 print 'Time: {0:.4f}m'.format((end - start) / 60)
 
@@ -79,19 +86,19 @@ annotations = coco_val.loadAnns(ids)
 
 img_list = []
 img_ids = []
-print "Testing: loading images"
+print "Testing: loading images and calculate image embeddings"
 for ann in annotations[:10]:
     file_name = coco_val.imgs[ ann['image_id'] ]['file_name']
     img = image.load_img('val2014/' + file_name, target_size=(224, 224))
-    img_list.append(image.img_to_array(img))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    features = net.predict(img)
+    features = features.reshape(img.shape[0], - 1)
+    img_list.append(features)
     img_ids.append(ann['image_id'])
 
-print "Testing: calculate image embeddings"
-start = time.time()
-img_features = image_features.vgg16_features(np.array(img_list))
-img_features = img_features[:,:K_IMG]
-end = time.time()
-print 'Time: {0:.4f}m'.format((end - start) / 60)
+img_features = np.array(img_list)
 
 tag_keys = []
 tag_list = []
@@ -103,9 +110,8 @@ for k in model.vocab.keys():
 print "Testing: prediction"
 N_RESULTS = 10
 pos = 0
-start = time.time()
-print img_features.shape
-for img in img_features:
+bar = progressbar.ProgressBar()
+for img in bar(img_features):
    v_img = np.dot(img, W_img)
    scores = []
    for tag in tag_list:
@@ -117,6 +123,3 @@ for img in img_features:
    for i in range(N_RESULTS):
        print tag_keys[ index[i] ]
    pos += 1
-
-end = time.time()
-print 'Time : {0:.4f}m'.format((end - start) / 60)
